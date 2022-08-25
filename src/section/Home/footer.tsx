9f2183f7ce0eb944b8836df6992aaabc4efabdb2
Wildan Najah Wildiansyah <wildan.najah@gmail.com>
2022-08-25 07:17:21 +0700

    footer responsive

--- src/section/Home/footer.tsx
+++ src/section/Home/footer.tsx
@@ -5,69 +5,72 @@ import {
   UilTwitter,
 } from "@iconscout/react-unicons";
 import typography from "../../theme/typography";
+import { MainContainer } from "./main";
 
 export default function Footer() {
   return (
-    <Stack marginBottom="3rem" spacing={5}>
-      <Stack
-        justifyContent="space-between"
-        direction={{ xs: "column", sm: "row" }}
-        spacing={5}
-      >
-        <Box>
-          <Typography
-            sx={{
-              ...typography.h3,
-            }}
-          >
-            Wildan
-          </Typography>
-          <Typography
-            sx={{
-              ...typography.subtitle2,
-            }}
-          >
-            Frontend Developer
-          </Typography>
-        </Box>
+    <MainContainer>
+      <Stack marginBottom="3rem" spacing={5}>
+        <Stack
+          justifyContent="space-between"
+          direction={{ xs: "column", sm: "row" }}
+          spacing={5}
+        >
+          <Box>
+            <Typography
+              sx={{
+                ...typography.h3,
+              }}
+            >
+              Wildan
+            </Typography>
+            <Typography
+              sx={{
+                ...typography.subtitle2,
+              }}
+            >
+              Frontend Developer
+            </Typography>
+          </Box>
 
-        <Stack direction="row" spacing={2}>
-          <Link
-            href="https://www.facebook.com/wildannajahw/"
-            target="_blank"
-            rel="noreferrer"
-            color="text.primary"
-          >
-            <UilFacebookF />
-          </Link>
-          <Link
-            href="https://www.instagram.com/wildannajahw/"
-            target="_blank"
-            rel="noreferrer"
-            color="text.primary"
-          >
-            <UilInstagram />
-          </Link>
-          <Link
-            href="https://twitter.com/wildannajahw"
-            target="_blank"
-            rel="noreferrer"
-            color="text.primary"
-          >
-            <UilTwitter />
-          </Link>
+          <Stack direction="row" spacing={2}>
+            <Link
+              href="https://www.facebook.com/wildannajahw/"
+              target="_blank"
+              rel="noreferrer"
+              color="text.primary"
+            >
+              <UilFacebookF />
+            </Link>
+            <Link
+              href="https://www.instagram.com/wildannajahw/"
+              target="_blank"
+              rel="noreferrer"
+              color="text.primary"
+            >
+              <UilInstagram />
+            </Link>
+            <Link
+              href="https://twitter.com/wildannajahw"
+              target="_blank"
+              rel="noreferrer"
+              color="text.primary"
+            >
+              <UilTwitter />
+            </Link>
+          </Stack>
         </Stack>
-      </Stack>
 
-      <Typography
-        sx={{
-          textAlign: "center",
-          ...typography.subtitle2,
-          color: "text.secondary",
-        }}
-      >
-        © Wildan. Allright reserved
-      </Typography>
-    </Stack>
+        <Typography
+          sx={{
+            textAlign: "center",
+            ...typography.subtitle2,
+            color: "text.secondary",
+          }}
+        >
+          © Wildan. Allright reserved
+        </Typography>
+      </Stack>
+    </MainContainer>
   );
 }
