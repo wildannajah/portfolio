feb0576fedd1c519969adfe381f549f168aee5cf
Wildan Najah Wildiansyah <wildan.najah@gmail.com>
2022-10-05 20:17:48 +0700

    hapus pokemon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,35 +2,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider as ReduxProvider } from "react-redux";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { store } from "../redux/store";
 import ThemeProvider from "../theme";
-// import offsetLimitPagination from "../utils/offsetLimitPagination";
 
-const client = new ApolloClient({
-  uri: "https://graphql-pokemon2.vercel.app/",
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          pokemons: {
-            merge(existing = [], incoming) {
-              return [...existing, ...incoming];
-            },
-          },
-        },
-      },
-    },
-  }),
-});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ReduxProvider store={store}>
       <ThemeProvider>
-        <ApolloProvider client={client}>
           <Component {...pageProps} />
-        </ApolloProvider>
       </ThemeProvider>
     </ReduxProvider>
   );
